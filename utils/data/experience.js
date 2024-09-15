670927d9c0a826173ec1d30100816b6c14be6827
shaikhashir8388 <shaikhashir871@gmail.com>
2024-09-15 16:38:11 +0500

    Added feature X

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,21 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Web developer lead',
-    company: "Help human rights",
-    duration: "( 2024 - Present)"
+    title: 'Mern stack developer',
+    company: "Qf Network",
+    duration: "( Sep 2024 - Present)"
+  },
+  {
+    id: 3,
+    title: "Web developer lead",
+    company: "Help human rights organizaion",
+    duration: "(May 2024 - Aug 2024)"
   },
   {
     id: 2,
     title: "Mern stack developer",
     company: "Coderatory",
-    duration: "(Sep 2023 - july 2024)"
+    duration: "(Sep 2023 - April 2024)"
   },
   {
     id: 3,
@@ -17,4 +23,6 @@ export const experiences = [
     company: "Precise Tech (Remote intern)",
     duration: "(Jan 2023 - Present)"
   }
+  
+  
 ]
