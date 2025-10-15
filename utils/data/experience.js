8f4fb595219cfe1665c659f346cb34e7f0486e09
shaikhashir8388 <shaikhashir871@gmail.com>
2025-10-15 18:32:59 +0500

    fixed

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,19 +3,19 @@ export const experiences = [
     id: 1,
     title: 'Software Engineer - (Remote) ',
     company: "eSanad - (UAE) ",
-    duration: "( Sep 2024 - Present)"
+    duration: "( Sep 2025 - Present)"
   },
   {
     id: 2,
     title: "MERN Stack Developer",
     company: "Qf Network - (Karachi, Pakistan)",
-    duration: "(Sep 2024 - March 2025)"
+    duration: "(Sep 2023 - March 2025)"
   },
   {
     id: 3,
-    title: "Next js Developer",
+    title: "Next js Developer(Intern)",
     company: "Coderatory - (Karachi, Pakistan)",
-    duration: "(Nov 2023 - August 2024)"
+    duration: "(april 2023 - August 2023)"
   }
   
   
